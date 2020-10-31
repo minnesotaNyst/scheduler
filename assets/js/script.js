39bbffb08bd20ce7d48224da44dad647de517af7
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-10-31 16:14:59 -0500

    finished the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,20 @@ for (i = 8; i < 18; i++) {
 	}
 	eCounter++;
 }
+//----GET LOCAL STORAGE----//
+var checkHistory = function () {
+	$('#text-entry1').val(localStorage.getItem('text-entry1'));
+	$('#text-entry2').val(localStorage.getItem('text-entry2'));
+	$('#text-entry3').val(localStorage.getItem('text-entry3'));
+	$('#text-entry4').val(localStorage.getItem('text-entry4'));
+	$('#text-entry5').val(localStorage.getItem('text-entry5'));
+	$('#text-entry6').val(localStorage.getItem('text-entry6'));
+	$('#text-entry7').val(localStorage.getItem('text-entry7'));
+	$('#text-entry8').val(localStorage.getItem('text-entry8'));
+	$('#text-entry9').val(localStorage.getItem('text-entry9'));
+	$('#text-entry10').val(localStorage.getItem('text-entry10'));
+};
+checkHistory();
 
 //---SAVE THE TEXT ENTRY TO LOCAL STORAGE----//
 $('button').click(function () {
@@ -25,16 +39,10 @@ $('button').click(function () {
 });
 
 //----LOAD THE PAGE----//
-
 var onLoad = function (list) {
 	//grab the current time
 	$('#currentDay').text(
 		cTime.format('dddd') + ', ' + cTime.format('MMM Do YYYY')
 	);
-	//create a loop to grab from local storage and display in the document
-	for (x = 1; x < 11; x++) {
-		$('text-entry' + x).val(localStorage.getItem('text-entry' + x));
-	}
 };
-
 onLoad();
